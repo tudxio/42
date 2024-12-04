94f715951b4eb4091dcf54d8283ed8385ab22f65
tudxio <41691329+tudxio@users.noreply.github.com>
2024-12-04 19:31:05 +0100

    Add files via upload

---
+++ libft/depot/ft_split.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobenard <lobenard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 16:00:59 by lobenard          #+#    #+#             */
+/*   Updated: 2024/11/26 15:21:40 by lobenard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	count_words(char const *s, char c)
+{
+	size_t	words;
+	size_t	i;
+
+	words = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			words++;
+		i++;
+	}
+	return (words);
+}
+
+static void	fill_tab(char *new, char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+}
+
+static void	set_mem(char **tab, char const *s, char c)
+{
+	size_t	count;
+	size_t	index;
+	size_t	i;
+
+	index = 0;
+	i = 0;
+	while (s[index])
+	{
+		count = 0;
+		while (s[index + count] && s[index + count] != c)
+			count++;
+		if (count > 0)
+		{
+			tab[i] = malloc(sizeof(char) * (count + 1));
+			if (!tab[i])
+				return ;
+			fill_tab(tab[i], (s + index), c);
+			i++;
+			index = index + count;
+		}
+		else
+			index++;
+	}
+	tab[i] = 0;
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	words;
+	char	**tab;
+
+	words = count_words(s, c);
+	tab = malloc(sizeof(char *) * (words + 1));
+	if (!tab)
+		return (NULL);
+	set_mem(tab, s, c);
+	return (tab);
+}
+/* #include <stdio.h>
+#include <stdlib.h>
+
+int	main() {
+    const char *original = "  Hello, world!  This is a test.  ";
+    const char charset = ' ';
+
+    char **split = ft_split(original, charset);
+
+    if (split == NULL) 
+	{
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    for (size_t i = 0; split[i] != NULL; i++)
+	{
+        printf("Word %zu: '%s'\n", i, split[i]);
+        free(split[i]);
+    }
+    free(split);
+
+    return 0;
+} */
---
+++ libft/depot/ft_strnstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobenard <lobenard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 14:31:40 by lobenard          #+#    #+#             */
+/*   Updated: 2024/11/26 18:26:11 by lobenard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	needle_len;
+	size_t	i;
+	size_t	j;
+
+	needle_len = ft_strlen(needle);
+	if (needle_len == 0)
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] && needle[j]
+			&& haystack[i + j] == needle[j] && i + j < len)
+		{
+			j++;
+		}
+		if (j == needle_len)
+			return ((char *)(haystack + i));
+		i++;
+	}
+	return (NULL);
+}
